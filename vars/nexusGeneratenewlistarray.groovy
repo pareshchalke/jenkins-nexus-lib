c0167ccbd9126fafe4394649f3263237ce939cab
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-07-04 14:18:33 +0100

    commit

--- vars/nexusGeneratenewlistarray.groovy
+++ vars/nexusGeneratenewlistarray.groovy
@@ -1,5 +1,6 @@
 def call ( String[] nexusrepo, String[] repoarray ) {
     def newlist = []
+    println "Generate new list array"
     println repoarray
     repoarray.each { String line ->
         value = nexusrepo.contains(line)
