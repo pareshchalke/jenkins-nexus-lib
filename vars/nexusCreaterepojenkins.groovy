76cecc00d9c027b28f7467a07e02c9ae04ba197a
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-07-04 14:21:42 +0100

    commit

---
+++ vars/nexusCreaterepojenkins.groovy
@@ -0,0 +1,13 @@
+def call ( String url, String gname, String repolist, String creds ) {
+  println "Createrepojenkins method"
+  println repolist
+  def nlist = nexusGetallrepo( url )
+  def glist = nexusGetgroup( url , gname )
+
+  def genlist = nexusGeneratenewlistarray ( nlist as String[], repolist as String[] )
+
+  genlist.each {
+    nexusAddrepo(it,url,creds)
+    nexusAddrepogroup(it,url,gname,creds)
+  }
+}
