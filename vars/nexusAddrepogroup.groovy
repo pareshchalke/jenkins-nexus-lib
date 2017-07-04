55c910706790857c43dc875262f4b8c5f2c29837
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-07-04 13:46:13 +0100

    commit

--- vars/nexusAddrepogroup.groovy
+++ vars/nexusAddrepogroup.groovy
@@ -1,9 +1,9 @@
 @NonCPS
 def call (String list, String url, String gname, String creds) {
-    members = nexusGetgroup(url,gname)
-    println "print Addrepogroup members"
-    println members
-    postdata = nexusGenerategrouppostdata(list,url,members,gname)
-    println postdata
-    postresponse = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', url: "${url}/service/local/repo_groups/${gname}", authentication: "${creds}", httpMode: 'PUT', requestBody: postdata
+  members = nexusGetgroup(url,gname)
+  println "print Addrepogroup members"
+  println members
+  postdata = nexusGenerategrouppostdata(list,url,members,gname)
+  println postdata
+  postresponse = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', url: "${url}/service/local/repo_groups/${gname}", authentication: "${creds}", httpMode: 'PUT', requestBody: postdata
 }
