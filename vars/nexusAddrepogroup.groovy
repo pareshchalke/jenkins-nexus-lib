2fdafc4fb9ee8d4aa6896c9788a96e2a66617ac0
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-07-04 13:38:34 +0100

    commit

--- vars/nexusAddrepogroup.groovy
+++ vars/nexusAddrepogroup.groovy
@@ -2,5 +2,6 @@
 def call (String list, String url, String gname, String creds) {
     def members = nexusGetgroup(url,gname)
     postdata = nexusGenerategrouppostdata(list,url,members,gname)
+    println postdata
     postresponse = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', url: "${url}/service/local/repo_groups/${gname}", authentication: "${creds}", httpMode: 'PUT', requestBody: postdata
 }
