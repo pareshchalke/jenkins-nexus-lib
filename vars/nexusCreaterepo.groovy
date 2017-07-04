302355bf5dc5bac779d9a104b3f63d03f41e516e
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-07-04 13:52:36 +0100

    commit

--- vars/nexusCreaterepo.groovy
+++ vars/nexusCreaterepo.groovy
@@ -3,6 +3,7 @@ def call ( String url, String gname, String repolist, String creds ) {
   def glist = nexusGetgroup( url , gname )
 
   def genlist = nexusGeneratenewlist ( nlist as String[], repolist )
+  println genlist
 
   genlist.each {
     nexusAddrepo(it,url,creds)
